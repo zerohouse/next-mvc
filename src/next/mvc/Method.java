e70cdb87a195db2b94aa7c141c4b52b64b36a891
park <park@192.168.0.6>
2015-05-18 10:43:31 +0900

    method

---
+++ src/next/mvc/Method.java
@@ -0,0 +1,8 @@
+package next.mvc;
+
+public class Method {
+	public static final String POST = "POST";
+	public static final String GET = "GET";
+	public static final String DELETE = "DELETE";
+	public static final String PUT = "PUT";
+}
